6d9686a890eedae63f5162c6f03f4a609414274c
Bridgette013 <BRITNELONG013@GMAIL.COM>
2025-12-03 00:54:07 -0700

    Remove unused Header import

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Privacy from './pages/Privacy';
 import Terms from './pages/Terms';
-import Header from './components/Header';
 import Footer from './components/Footer';
 
 const App = () => {
     return (
         <div>
-            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/privacy" element={<Privacy />} />
